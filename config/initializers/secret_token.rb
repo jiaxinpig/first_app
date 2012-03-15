01cb8f3bd66746429166cd8daffc478a207d5097
U-FAREAST\chuanye <chuanye@CHUANYE_NB.fareast.corp.microsoft.com>
2012-03-15 23:42:25 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '11aad650c6f95de659c1237fe383f10ebade0a3180fec93a62e0cbc373131aea8c5062033412fb8eac37538792877c577f7c4989b4f746cff85be01808a946b6'
